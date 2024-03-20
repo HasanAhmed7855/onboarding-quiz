c2717dacd5c9ee5aec77e01dbda1957044da7ffe
Hasan Ahmed <hmedzzh@amazon.com>
2024-03-20 20:21:12 +0000

    introducing nextauth to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app"
+import { SessionProvider } from "next-auth/react"
 import style from "../styles/Global.module.css"
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
   return (
-    <div  className={style.globalPageCenter}>
-      <h1 className={style.appTitle}>Onboarding Quiz Manager</h1>
-      <Component {...pageProps} />
-    </div>
+    <SessionProvider session={session}>
+      <div  className={style.globalPageCenter}>
+        <h1 className={style.appTitle}>Onboarding Quiz Manager</h1>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
   )
 }
--- src/pages/main-menu-page/main-menu-component.tsx
+++ src/pages/main-menu-page/main-menu-component.tsx
@@ -4,8 +4,33 @@ import Link from "next/link"
 import { useEffect, useState } from "react"
 import globalStyle from "../../styles/Global.module.css"
 import style from "../../styles/Mainmenu.module.css"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function MainMenuComponent() {
+  const { data: session, status } = useSession()
+  const usersName = session?.user?.name
+
+  if (status === "loading") {
+    return <p>Hang on there...</p>
+  }
+
+  if (status === "authenticated") {
+    return (
+      <>
+        <p>Signed in as {usersName}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+        <img src="https://cdn.pixabay.com/photo/2017/08/11/19/36/vw-2632486_1280.png" />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <p>Not signed in.</p>
+      <button onClick={() => signIn("github")}>Sign in</button>
+    </>
+  )
+
   const [isAdmin, setIsAdmin] = useState<boolean>(false) 
 
   useEffect(() => {
