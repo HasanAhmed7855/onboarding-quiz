9eb2bd66f5c54fdbde00966ca2a123441f97cd79
Hasan Ahmed <hmedzzh@amazon.com>
2024-03-21 19:20:02 +0000

    removing user from db again

--- src/pages/api/login-verification.ts
+++ src/pages/api/login-verification.ts
@@ -12,6 +12,31 @@ export default async function handle(
   const { username, password } = req.body
 
   try {
+
+    const deletedQuiz = await prisma.$transaction(async (prisma) => { // transaction ensures the deletes are executes together, not seperately
+          
+      await prisma.session.delete({
+        where: {
+          id: "clu1jb0o200014ibhv2vea209"
+        }
+      })
+
+      await prisma.account.delete({
+        where: {
+          id: "clu1m7cvb0002s3hl15wwvedl"
+        }
+      })
+
+      return prisma.user.delete({
+        where: {
+          email: "hasanahmed7855@hotmail.com"
+        }
+      })
+  })
+
+  if (deletedQuiz) {
+      return res.status(200).json({ message: 'Quiz successfully deleted' })
+  }
   return res.status(200).json({ message: 'Quiz successfully' })
   
     /*
