d8bc98303c644f746cab145284452cf8b06ce194
Hasan Ahmed <hmedzzh@amazon.com>
2024-03-21 14:26:11 +0000

    new prisma schema wihhout access level

---
+++ prisma/migrations/20240321142439_model_without_accesslevel/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `access_level` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "access_level";
