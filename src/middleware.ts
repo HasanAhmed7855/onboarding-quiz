ab24ded7b84892184cd7cfa20dd795f5fb57a6ce
Hasan Ahmed <hmedzzh@amazon.com>
2024-03-23 12:30:22 +0000

    adding secret to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,4 +3,5 @@ export { default } from "next-auth/middleware"
 export const config = {
     // Dont run authentication on app entry page
     matcher: ["/((?!$).*)"],
+    secret: process.env.NEXTAUTH_SECRET
   };
