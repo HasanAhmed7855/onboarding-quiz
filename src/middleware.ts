634c3462304626f3c7713c251eaa1c2fd1e159e3
Hasan Ahmed <hmedzzh@amazon.com>
2024-03-24 18:36:38 +0000

    middleware - going back to documented method

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,6 @@
-import withAuth from "next-auth/middleware";
-
-export default withAuth({
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+export { default } from "next-auth/middleware"
 
   export const config = {
+    //secret: process.env.NEXTAUTH_SECRET,
     matcher: ["/((?!$).*)"],
   };
