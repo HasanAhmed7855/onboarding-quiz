e28cd27b13ff998aab83d985c6c219b68ef82943
Hasan Ahmed <hmedzzh@amazon.com>
2024-03-24 19:18:36 +0000

    getting secret from env folder

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 export { default } from "next-auth/middleware"
 
   export const config = {
-    //secret: process.env.NEXTAUTH_SECRET,
-    secret: "88962134c21d96c282b95859b6cd962c",
+    secret: process.env.NEXTAUTH_SECRET,
+    //secret: "88962134c21d96c282b95859b6cd962c",
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico|$).*)'],
   };
