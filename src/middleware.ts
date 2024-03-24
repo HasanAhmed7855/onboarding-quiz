cd3c73bc7ed9ee62196f06418830d22456f97ba7
Hasan Ahmed <hmedzzh@amazon.com>
2024-03-24 19:14:15 +0000

    adding secret manually to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 export { default } from "next-auth/middleware"
 
   export const config = {
-    secret: process.env.NEXTAUTH_SECRET,
+    //secret: process.env.NEXTAUTH_SECRET,
+    secret: "88962134c21d96c282b95859b6cd962c",
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico|$).*)'],
   };
