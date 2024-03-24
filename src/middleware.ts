22f8eff3fe73468073f8cc1c06feed7f87ad99fa
Hasan Ahmed <hmedzzh@amazon.com>
2024-03-24 19:35:38 +0000

    just matcher in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 export { default } from "next-auth/middleware"
 
-  export const config = {
-    secret: process.env.NEXTAUTH_SECRET,
-    //secret: "88962134c21d96c282b95859b6cd962c",
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|$).*)'],
-  };
+export const config = {
+    //secret: process.env.NEXTAUTH_SECRET,
+    matcher: ['/((?!$).*)',],
+};
+
+
