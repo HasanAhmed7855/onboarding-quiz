e339346400aa3aea176ca6eb2b252bd9b7173b03
Hasan Ahmed <hmedzzh@amazon.com>
2024-03-27 13:43:29 +0000

    adding first cypress test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+
+    },
   },
 });
--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -1,9 +1,17 @@
 describe('The Home Page', () => {
     it('successfully loads with the correct contents', () => {
-      cy.visit('/')
+      cy.visit('http://localhost:3000')
 
-      //cy.contains('h1', 'Onboarding Quiz Manager')
-      //cy.contains('a', 'Login')
+      cy.contains("Login using GitHub").click()
+      cy.get('.button').click()
+      cy.get('.button').click()
+
+      cy.get('input#login_field').type(Cypress.env("GITHUB_USER"));
+      cy.get('input#password').type(Cypress.env("GITHUB_PASSWORD"));
+      cy.get('input[type="submit"]').click();
+
+    
     })
+
   })
   
